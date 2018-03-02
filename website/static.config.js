bd5a7b79637b86c4179c7491d7349e34c48a573b
Valentin Huber <valentin.huber@msg.group>
2018-03-02 11:12:10 +0100

    Added missing siteRoot

--- website/static.config.js
+++ website/static.config.js
@@ -6,6 +6,7 @@ const typescriptWebpackPaths = require('./webpack.config.js')
 
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
+  siteRoot: 'https://nodeschool.io/passau/',
   getSiteProps: () => ({
     title: 'NodeSchool Passau',
   }),
