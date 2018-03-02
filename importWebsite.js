e7bcb5cbe02151787a5e90619d63a2fc5532b992
Valentin Huber <valentin.huber@msg.group>
2018-03-02 12:31:44 +0100

    Cleaned up

--- importWebsite.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-
-if (!shell.test('-e', './website/dist/index.html')) {
-    console.error('Run yarn build in the master branch first to build the static website');
-    console.error('Then switch to the gh-pages branch and run this script');
-    process.exit(1);
-}
-
-shell.cp('-Rf', 'website/dist/*', '.');
