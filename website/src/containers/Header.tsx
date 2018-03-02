dc4e4b624108ef6e4e5e784a88bd14a5d641b050
Valentin Huber <valentin.huber@msg.group>
2018-03-02 12:55:10 +0100

    Removed logo for testing

--- website/src/containers/Header.tsx
+++ website/src/containers/Header.tsx
@@ -5,7 +5,7 @@ export default class Header extends React.PureComponent<{}, {}>{
     render() : React.ReactNode {
         return (
             <header id="header">
-                <img role="presentation" src='/static/nodeschool_passau.svg' alt="nodeschool passau avatar"/>
+                {/* <img role="presentation" src='/static/nodeschool_passau.svg' alt="nodeschool passau avatar"/> */}
                 <div className="hero">
                     <div className="hero-body">
                         <div className="container has-text-centered">
