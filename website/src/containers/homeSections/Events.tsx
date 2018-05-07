9bea2c367f3971073e14b16ebcfb25465950e961
Valentin Huber <valentin.huber@msg.group>
2018-05-07 14:39:36 +0200

    Removed past event

--- website/src/containers/homeSections/Events.tsx
+++ website/src/containers/homeSections/Events.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import SectionComponent from '../Section';
 import { Title, Half, Card } from '../Utility';
 
-export default class Events extends SectionComponent{
+export default class Events extends SectionComponent {
 
     static readonly id = "events";
     static readonly title = "Events";
 
-    render() : React.ReactNode{
+    render(): React.ReactNode {
         return <Half>
-                <Title>Upcoming events</Title>
-                <Card header="Next workshop: Friday, 4th of May, 13:00 - 16:00">
-                    <p>Check out the registration pages for <a href="https://www.eventbrite.de/e/nodeschool-passau-javascript-nodejs-for-beginners-tickets-45189733710">
-                    beginners</a> and <a href="https://www.eventbrite.de/e/nodeschool-passau-javascript-nodejs-for-intermediate-tickets-45188647461">intermediate</a> participants!</p>
-                   <p>Participation and WIFI is for free, as usual.</p>
-                </Card>
-            </Half>;
+            <Title>Upcoming events</Title>
+            <Card header="Nothing planned at the moment - stay tuned">
+                <footer className="card-footer">
+                    Past events:
+                    <ul>
+                        <li>2018-05-04</li>
+                    </ul>
+                </footer>
+            </Card>
+        </Half>;
     }
 }
