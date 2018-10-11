b335a9a20c45d975c6ae5944e27ed18e3324b538
Valentin Huber <valentin.huber@msg.group>
2018-10-11 16:18:27 +0200

    Added new event

--- website/src/containers/homeSections/Events.tsx
+++ website/src/containers/homeSections/Events.tsx
@@ -10,13 +10,23 @@ export default class Events extends SectionComponent {
     render(): React.ReactNode {
         return <Half>
             <Title>Upcoming events</Title>
-            <Card header="Nothing planned at the moment - stay tuned">
+            <Card>
+                <div className="content">
+                    <p className="title" style={{ color: 'black' }}>Next workshop: <time dateTime="2018-11-20">20th of November</time></p>
+                    <p className="subtitle" style={{ color: 'black' }}>
+                        Register <a
+                            style={{ color: 'black' }}
+                            target="_blank"
+                            href="https://www.eventbrite.de/e/nodeschool-passau-javascript-nodejs-tickets-51193225315">here</a> for
+                            the next workshop about writing command line tools with Node.js</p>
+                </div>
                 <footer className="card-footer">
                     Past events:
                     <ul>
                         <li>2018-05-04</li>
                     </ul>
                 </footer>
+
             </Card>
         </Half>;
     }
