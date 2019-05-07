055e665b044c21f04660afd7b12078fbca277577
Valentin Huber <valentin.huber@msg.group>
2019-05-07 09:40:31 +0200

    Added event at 21.5.2019

--- website/src/containers/homeSections/Events.tsx
+++ website/src/containers/homeSections/Events.tsx
@@ -14,7 +14,7 @@ export default class Events extends SectionComponent {
                 <div className="content">
                     <p className="title" style={{ color: 'black' }}>Next workshop: <time dateTime="2019-05-21">21th of May</time></p>
                     <p className="subtitle" style={{ color: 'black' }}>
-                        Stay tuned for more information
+                        More details and registration at the <a href="https://www.meetup.com/de-DE/Nodeschool-Passau/events/260909843/">meetup event page</a>
                     </p>
                 </div>
                 <footer className="card-footer">
